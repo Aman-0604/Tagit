37332feefa84857fcee392d64240587e7134810c
Aman Gupta <techbugaman@gmail.com>
2022-09-16 17:57:44 +0530

    User Authentication is now completed, now we have to shift our focus to populate correct data to the desired user

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,10 +1,23 @@
-import React from 'react'
+import React,{useEffect} from 'react'
 import Feed from './Feed';
 import LeftContentHome from './LeftContentHome';
 import MiddleContentTopFeedHome from './MiddleContentTopFeedHome';
 import RightContentHome from './RightContentHome';
+import { useNavigate } from 'react-router-dom';
+
 
 export default function Home() {
+  let navigate = useNavigate();
+
+  useEffect(() => {
+    if (localStorage.getItem('token')) {
+      console.log(localStorage.getItem('token'));
+    }
+    else {
+      navigate("/login");
+    }
+    // eslint-disable-next-line
+  }, [])//[]means sirf ek baar yeh function chalega
   return (
     <div className='container d-flex flex-row justify-content-center' style={{width:"85%",marginTop:"25px"}}>
         <div className="left_home_container d-flex justify-content-center" style={{width: "20%",height:"300px",padding:"10px",margin:"15px 10px",borderRadius:"10px",backgroundColor:"#212529",position:"sticky",top:"9%"}}>
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -45,11 +45,11 @@ export default function Navbar() {
                             </li>
                             <li className="nav-item dropdown">
                                 <Link className={`nav-link dropdown-toggle ${location.pathname === "/profile" ? "active" : ""}`} style={{ fontSize: "18px" }} to="/profile" role="button" data-bs-toggle="dropdown" aria-expanded="false">Profile</Link>
-                                <ul className="dropdown-menu">
-                                    <li><Link className="dropdown-item" to="/">Action</Link></li>
-                                    <li><Link className="dropdown-item" to="/">Another action</Link></li>
+                                <ul className="dropdown-menu" style={{backgroundColor: "#212529"}}>
+                                    <li><Link className="dropdown-item" to="/" style={{color : "white"}}>View Profile</Link></li>
+                                    <li><Link className="dropdown-item" to="/" style={{color : "white"}}>Settings</Link></li>
                                     <li><hr className="dropdown-divider"/></li>
-                                    <li><Link className='dropdown-item' to="/login"><button type="button" onClick={logoutHandler} className="btn" style={{ backgroundColor: "#7532F9", color: "white" }}>Logout</button></Link></li>
+                                    <li><Link className='dropdown-item' to="/login"><button type="button" onClick={logoutHandler} className="btn" style={{ backgroundColor: "#212529", color: "white", marginLeft:"-10px" }}>Logout</button></Link></li>
                                 </ul>
                             </li>
                         </ul>
